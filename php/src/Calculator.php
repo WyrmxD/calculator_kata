948f4632a0253f98eb38f33b1aed1af23b9dd267
Iñaki Torres <wyrmxd@gmail.com>
2014-12-28 17:16:12 +0000

    Using our own separators

--- php/src/Calculator.php
+++ php/src/Calculator.php
@@ -2,18 +2,22 @@
 
 class Calculator {
 	
-	static function add($string){
-		
-		$separators = '/[,|\s]/';
-		if (strstr($string, "//")){
-			$separators = "/" . substr($string, 2, 1) . "/";
-		}
-		$numbers = preg_split($separators, $string);			
+	static function add($string) {
+
+		$numbers = self::splitter($string);			
 		
 		$total = 0;
 		foreach ($numbers as $number) {
 			$total += $number;
 		}
 		return $total;
+	}
+
+	private function splitter($string) {
+		$separators = '/[,|\s]/';
+		if (strstr($string, "//")){
+			$separators = "/" . substr($string, 2, 1) . "/";
+		}
+		return preg_split($separators, $string);
 	}
 }
